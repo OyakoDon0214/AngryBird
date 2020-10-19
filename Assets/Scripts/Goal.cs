4690086b66a578a9d425e468f5770588e66c934f
y-shoji <p108053y@mail.kyutech.jp>
2020-10-19 09:50:52 +0900

    とりあえず周平のを取り込んだだけ

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    float press = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (press > 20.0) {
+            //スコア処理を追加
+//            FindObjectOfType<Score>().AddPoint(10);
+//            ball.score += 100;
+            GameObject.Destroy (gameObject);
+        }
+    }
+
+    void OnCollisionStay(Collision collision) {
+        if (collision.gameObject.tag == "block") {
+            Vector3 v = collision.rigidbody.velocity - GetComponent<Rigidbody> ().velocity;
+            float p = Mathf.Sqrt (v.x * v.x + v.y * v.y);
+            press +=  p;
+//            ball.score += p;
+            Debug.Log(collision.rigidbody.velocity);
+        }
+    }
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //ballプレハブ
+    public GameObject ball;
+    //弾が生成されるポジションを保有するゲームオブジェクト
+    public GameObject ballPos;
+
+    Vector3 forceDirection;
+
+    Vector3 force;
+
+     // 上の向きに加わる力の大きさを定義
+    [SerializeField]
+    public float forceMagnitude = 10.0f;
+
+    // X軸からの角度
+    [SerializeField]
+    public float forceAngle = 45.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スペースが押されたとき
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot(forceAngle, forceMagnitude);
+        }
+    }
+
+    public void shoot(float forceAngle, float forceMagnitude){
+        //ballをインスタンス化して発射
+        GameObject createdBall = Instantiate(ball) as GameObject;
+        createdBall.transform.position = ballPos.transform.position;
+
+        // 入力された角度をラジアンに変換
+        float rad = forceAngle * Mathf.Deg2Rad;
+
+        // それぞれの軸の成分を計算
+        float x = Mathf.Cos(rad);
+        float y = Mathf.Sin(rad);
+        float z = 0.0f;
+
+        // Vector3型に格納
+        forceDirection = new Vector3(x, y, z);
+
+        // 向きと大きさからSphereに加わる力を計算する
+        force = forceMagnitude * forceDirection;
+
+        // Rigidbodyに力を加えて発射
+        createdBall.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+
+    }
+
+}

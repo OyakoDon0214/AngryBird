0379c20248ea4e6d5f0fec0c7e787057d50283de
y-shoji <p108053y@mail.kyutech.jp>
2020-10-26 16:34:39 +0900

    pressによって点数変える処理

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,7 +6,12 @@ public class Block : MonoBehaviour
 {
     float press = 0;
 
-    public GameObject scoreNumber;
+    public GameObject score300;
+    public GameObject score100;
+    public GameObject score50;
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,19 +22,7 @@ public class Block : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (press > 10.0) {
-            //スコア処理を追加
-//            FindObjectOfType<Score>().AddPoint(10);
-//            ball.score += 100;
-            ScoreManager.score_num += 100f;
-            //スコアの数字を表示
-            //スコアの数字を表示
-            GameObject scoreNumberCopy = Instantiate(scoreNumber) as GameObject;
-            scoreNumberCopy.transform.position = this.transform.position;
-            GameObject.Destroy (scoreNumberCopy,1.5f);
-            GameObject.Destroy (gameObject);
-            
-        }
+       
     }
     void OnCollisionStay(Collision collision) {
         if (collision.gameObject.tag == "block"|collision.gameObject.tag =="ball") {
@@ -37,6 +30,30 @@ public class Block : MonoBehaviour
             float p = Mathf.Sqrt (v.x * v.x + v.y * v.y);
             press +=  p;
             Debug.Log("press"+press);
+            
+        }
+         if(1<press && press<5){
+            ScoreManager.score_num += 50f;
+            GameObject scoreNumberCopy = Instantiate(score50) as GameObject;
+            scoreNumberCopy.transform.position = this.transform.position;
+            GameObject.Destroy (scoreNumberCopy,1.5f);
+        }
+        if(5<press && press<10.0){
+            ScoreManager.score_num += 100f;
+            GameObject scoreNumberCopy = Instantiate(score100) as GameObject;
+            scoreNumberCopy.transform.position = this.transform.position;
+            GameObject.Destroy (scoreNumberCopy,1.5f);
+        }
+        if (press > 10.0) {
+            //スコア処理を追加
+//            FindObjectOfType<Score>().AddPoint(10);
+//            ball.score += 100;
+            ScoreManager.score_num += 300f;
+            //スコアの数字を表示
+            GameObject scoreNumberCopy = Instantiate(score300) as GameObject;
+            scoreNumberCopy.transform.position = this.transform.position;
+            GameObject.Destroy (scoreNumberCopy,1.5f);
+            GameObject.Destroy (gameObject);
             
         }
     }
